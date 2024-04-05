b9f5bb4d2b4b5673baec0c1ae8f73cf26dbb23cb
linhnguyen2803 <nguyenlinh8203@gmail.com>
2024-04-05 17:07:24 +0700

    Delete extra lines of code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -149,12 +149,7 @@ export default function Home() {
             <div className="container max-w-screen-xl mx-auto px-4">
 
               <h1 className="font-semibold text-neutral-50 text-3xl md:text-4xl text-center mb-5">Summary</h1>
-
-<<<<<<< HEAD
               <p className="font-normal text-gray-200 text-md md:text-lg text-center mb-20">The FIB token on the Binance Smart Chain platform provides a solution for connecting customers and professional financial organizations globally. With high liquidity and ownership rights offering benefits, FIB tokens create a safe, convenient, and transparent environment for connection and transactions on the blockchain network.</p>
-=======
-              <p className="font-normal text-gray-500 text-md md:text-lg text-center mb-20">The FIB token on the Binance Smart Chain platform provides a solution for connecting customers and professional financial organizations globally. With high liquidity and ownership rights offering benefits, FIB tokens create a safe, convenient, and transparent environment for connection and transactions on the blockchain network.</p>
->>>>>>> 86ee87415e39e74fd4080643b48deede2221d06f
             </div>
           </section>
 
@@ -324,11 +319,7 @@ export default function Home() {
                                 </h1>
 
                                 {/* <!-- Title --> */}
-<<<<<<< HEAD
                                 <div className="text-gray-400 font-light mb-2">
-=======
-                                <div className="text-gray-700 font-light mb-2">
->>>>>>> 86ee87415e39e74fd4080643b48deede2221d06f
                                 Marketing & Community
                                 </div>
 
@@ -371,11 +362,8 @@ export default function Home() {
                                 </h1>
 
                                 {/* <!-- Title --> */}
-<<<<<<< HEAD
                                 <div className="text-gray-400 font-light mb-2">
-=======
-                                <div className="text-gray-700 font-light mb-2">
->>>>>>> 86ee87415e39e74fd4080643b48deede2221d06f
+
                                 IT Solution
 
                                 </div>
