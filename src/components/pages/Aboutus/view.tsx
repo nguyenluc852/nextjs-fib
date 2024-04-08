5d1c48eea75d363a1e2b0c1c99b216989038f704
linhnguyen2803 <nguyenlinh8203@gmail.com>
2024-04-08 21:45:58 +0700

    change text

--- src/components/pages/Aboutus/view.tsx
+++ src/components/pages/Aboutus/view.tsx
@@ -34,27 +34,27 @@ export const view = (useService: UseService) => {
         <div className="responsive">
           <div className="container">
              <div className="mt-3">
-                <span className="text-[50px] leading-[44px] font-normal bg-gradient-to-r text-transparent bg-clip-text from-[#8D42E7] via-[#E7429B] to-[#E7A542]">
+                <span className="text-[50px] leading-[44px] font-bold bg-gradient-to-r text-transparent bg-clip-text from-[#8D42E7] via-[#E7429B] to-[#E7A542]">
                 Digital era Web3
                 </span>
                 <div className="grid grid-cols-10 gap-3">
                     <div className="col-span-6">
                       <div className="mt-5">
                         <div className="ml-[30px]">
-                          <p className="font-normal text-[25px] text-neutral-50">
+                          <p className="font-bold text-[25px] text-neutral-50">
                           Web3 is having a profound impact on the financial sector, opening up 
                           new opportunities and changing the way we approach and interact with 
                           finance. These potentials have not yet been fully exploited and there 
                           are many aspects left to explore and develop in the future.
                           </p>
-                          <p className="mt-2 font-normal text-[25px] text-neutral-50">
+                          <p className="mt-2 font-bold text-[25px] text-neutral-50">
                           The FIB token was created to leverage the potential of Web3 in the 
                           financial sector by providing a decentralized, fair and transparent 
                           financial ecosystem. The FIB token allows users to access financial 
                           services easily and securely, while creating investment and financial 
                           opportunities for those with limited access to products and services.
                           </p>
-                          <p className="font-normal text-[25px] text-neutral-50 mt-5">
+                          <p className="font-bold text-[25px] text-neutral-50 mt-5">
                           Discover now 
                           </p>
                         </div>
